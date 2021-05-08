efa53eb66a8e78488ed4f98b7c59629bbb2f5ebf
Souparna Pal <76830721+SpideyCoding@users.noreply.github.com>
2021-05-08 11:52:15 +0530

    Add files via upload

---
+++ script/main.js
@@ -0,0 +1,23 @@
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+  let myName = prompt('Please enter your name.');
+  if(!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Hello, ' + storedName + ' Welcome to The Official Hogwarts Website ';
+  }
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Hello, ' + storedName + ', Welcome to The Official Hogwarts Website ';
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
